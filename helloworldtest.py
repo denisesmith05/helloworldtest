379e2c40369aa74d1fe0cabab98545a64e35b391
Denise <denise.sunshine@icloud.com>
2024-09-19 17:30:42 -0400

    first commit

---
+++ helloworldtest.py
@@ -0,0 +1,4 @@
+print('****************')
+print('* Hello World! *')
+print('****************')
+print("I'm Denise Smith, and I'm originally from Colorado, but I moved to California about a year ago! I love hiking, dancing, and working out; any activity I can do with family and friends!")
